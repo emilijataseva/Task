47d60fb6baea65b55d5fd6e09efed53412acba3d
Emilija <emilijataseva23@gmail.com>
2018-07-23 01:30:36 +0200

    Task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import {HttpClientModule} from '@angular/common/http';
+import { PhoneNumberValidatorDirective } from './num-validation.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserFormComponent,
+    PhoneNumberValidatorDirective,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
